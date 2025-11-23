0ca1cdae86f1091ad72b4754e9f0e8ed725ed6a1
Jan Navarro <42248628+navarrojandg@users.noreply.github.com>
2025-11-22 20:47:52 -0800

    Add puppeteer to worker

---
+++ apps/worker/src/lib/puppeteer.ts
@@ -0,0 +1,39 @@
+// @ts-ignore
+import chromePaths from "chrome-paths";
+import puppeteer, { Page } from "puppeteer-core";
+
+export async function withPage(
+  callback: (page: Page) => Promise<any>,
+  { headless = true, autoCloseBrowser = true } = {}
+) {
+  const browser = await puppeteer.launch({
+    executablePath:
+      chromePaths.chrome ||
+      (!!chromePaths.chromium
+        ? "/usr/bin/chromium-browser"
+        : "chromium-browser"),
+    headless,
+    args: ["--no-sandbox"],
+  });
+
+  let res;
+
+  try {
+    const page = await browser.newPage();
+
+    // Close the first page by default
+    await (await browser.pages())[0]?.close();
+
+    res = await callback(page);
+  } catch (err) {
+    console.error(err);
+
+    throw err;
+  } finally {
+    if (autoCloseBrowser) {
+      await browser.close();
+    }
+  }
+
+  return res;
+}
